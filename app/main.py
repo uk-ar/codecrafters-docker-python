08f226be81d355c859755e88f68094307ec6fa69
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-07-18 19:58:31 +0000

    Enable exit code

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"), end="")
     print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end="")
-
+    exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()

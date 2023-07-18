f816be512ffc547fc2025fc66ff03583509bcaf6
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-07-18 21:38:48 +0000

    Enable process isolation

--- app/main.py
+++ app/main.py
@@ -2,6 +2,16 @@ import subprocess
 import sys
 import os
 import shutil
+import ctypes
+
+# 名前空間の種類を表すフラグ
+CLONE_NEWNS = 0x00020000  # マウント名前空間
+CLONE_NEWUTS = 0x04000000  # UTS名前空間
+CLONE_NEWIPC = 0x08000000  # IPC名前空間
+CLONE_NEWUSER = 0x10000000  # ユーザー名前空間
+CLONE_NEWPID = 0x20000000  # PID名前空間
+CLONE_NEWNET = 0x40000000  # ネットワーク名前空間
+CLONE_NEWCGROUP = 0x02000000  # cgroup名前空間
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,6 +26,13 @@ def main():
     os.mkdir("./tmp")
     os.makedirs("./tmp"+os.path.dirname(command))
     shutil.copy2(command,"./tmp"+command)
+
+    # unshareシステムコールを呼び出す
+    libc = ctypes.CDLL('libc.so.6')
+    result = libc.unshare(CLONE_NEWPID)
+
+    if result != 0:
+        raise OSError(ctypes.get_errno())
 
     completed_process = subprocess.run(["chroot","./tmp",command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"), end="")
